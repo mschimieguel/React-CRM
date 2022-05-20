2d53831545c22648816337cfae9ec928260db781
klysman08 <klysman.rk@gmail.com>
2022-05-20 19:04:24 -0300

    up app index

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -2,19 +2,11 @@ import * as React from 'react';
 
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
-import IconLabelTabs from './components/IconLabelTabs';
 import VerticalTabs from './components/VerticalTabs';
 import SearchAppBar from './components/SearchAppBar';
 
-/* 
-import BasicModal from './components/BasicModal';
-import BasicList from './components/BasicList';
-import MediaCard from './components/MediaCard';
-import BasicGrid from './components/BasicGrid';
- */
-
-/* import Home from './pages/Home';
-import Overview from './pages/Overview'; */
+import Home from './pages/Home';
+import Overview from './pages/Overview'; 
 
 function App() {
   return (
@@ -27,8 +19,8 @@ function App() {
               <Routes>
                 <Route exact path='/' element={<Home />} />
                 <Route exact path='/Overview' element={<Overview />} />
-              </Routes>
-            */}
+              </Routes> */}
+           
 
 
             <SearchAppBar /> {/* menu horizontal superior */}
