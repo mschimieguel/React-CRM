85a1cd0f56eeb92c4afa35c9b6330582e0125460
mschimieguel <matheussilva@dcc.ufmg.br>
2022-08-23 00:11:06 -0300

    Vertical Tabs Refactoring

--- frontend-new/src/pages/Calendar.js
+++
@@ -1,9 +0,0 @@
-function Calendar (){
-    return <div>
-        <h1>Calendar</h1>
-        <p>
-            Incididunt consectetur id nulla proident ipsum commodo. </p>
-    </div>
-}
-
-export default Calendar;
